5e2927874ddc0a11f2d9d98fbb7a1b52e1ed0fd3
Parth Vyas <parth@nclouds.com>
2022-06-06 15:33:22 +0530

    add release version management commands to release script
    Signed-off-by: Parth Vyas <parth@nclouds.com>

--- deploy_scripts/release.sh
+++ deploy_scripts/release.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
-CURRENT_VERSION=$(grep "NOPS_FORWARDER_VERSION" settings.py | awk -v RS='"' '!(NR%2)')
-PACKAGE_NAME="nops-aws-forwarder-deployment-package-${CURRENT_VERSION}.zip"
+OLD_VERSION=$(grep "NOPS_FORWARDER_VERSION" settings.py | awk -v RS='"' '!(NR%2)')
+NEW_VERSION=$(cat Release_Version)
+PACKAGE_NAME="nops-aws-forwarder-deployment-package-${NEW_VERSION}.zip"
 BUNDLE_PATH="./.package/${PACKAGE_NAME}"
+
+#### update files with new release version
+sed -i -r "s/${OLD_VERSION}/${NEW_VERSION}/g" deploy_cloudformation/lambda-forwarder-cloudformation-template.yaml
+sed -i -r "s/${OLD_VERSION}/${NEW_VERSION}/g" Release_Version
+
+#### create github release
 gh release create ${PACKAGE_NAME} $BUNDLE_PATH
 
-aws s3 cp deploy_cloudformation/lambda-forwarder-cloudformation-template.yaml s3://nops-cloudformation-template/lambda-forwarder-cloudformation-template-${CURRENT_VERSION}.yaml
+# #### upload new release cloudformation templates to s3 bucket
+aws s3 cp deploy_cloudformation/lambda-forwarder-cloudformation-template.yaml s3://nops-cloudformation-template/lambda-forwarder-cloudformation-template-${NEW_VERSION}.yaml
 aws s3 cp deploy_cloudformation/lambda-forwarder-cloudformation-template.yaml s3://nops-cloudformation-template/lambda-forwarder-cloudformation-template.yaml
