fb01552b8a17fd63cb2bfa89dbd3700ccafe63a1
Le Kien Truc <truc@nops.io>
2022-06-17 14:09:36 +0700

    Bump version from 1.0.6 to 1.0.7

--- settings.py
+++ settings.py
@@ -1,7 +1,13 @@
+import base64
 import logging
 import os
 
+import boto3
+
 logger = logging.getLogger()
+
+
+boto3_config = botocore.config.Config(connect_timeout=5, read_timeout=5, retries={"max_attempts": 2})
 
 
 def get_env_var(envvar, default, boolean=False):
@@ -21,7 +27,7 @@ NOPS_NO_SSL = False
 NOPS_PORT = "443"
 NOPS_SKIP_SSL_VALIDATION = True
 NOPS_URL = "app.nops.io"
-NOPS_FORWARDER_VERSION = "1.0.6"
+NOPS_FORWARDER_VERSION = "1.0.7"
 
 CT_EVENT_TYPES = [
     "RunInstances",
@@ -35,17 +41,33 @@ CT_EVENT_TYPES = [
 ]
 
 
-if "NOPS_API_KEY" in os.environ:
-    logger.debug("Fetching the Datadog API key from environment variable NOPS_API_KEY")
-    NOPS_API_KEY = os.environ["NOPS_API_KEY"]
-
 if "NOPS_URL" in os.environ:
-    logger.debug("Fetching the Datadog API key from environment variable NOPS_URL")
+    logger.debug("Fetching the API key from environment variable NOPS_URL")
     NOPS_URL = os.environ["NOPS_URL"]
 
 if "NOPS_PORT" in os.environ:
-    logger.debug("Fetching the Datadog API key from environment variable NOPS_PORT")
+    logger.debug("Fetching the API key from environment variable NOPS_PORT")
     NOPS_PORT = os.environ["NOPS_PORT"]
 
 if "NOPS_SKIP_SSL_VALIDATION" in os.environ:
     NOPS_SKIP_SSL_VALIDATION = os.environ["NOPS_SKIP_SSL_VALIDATION"].upper() == "TRUE"
+
+
+if (
+    "NOPS_KMS_API_KEY" in os.environ
+    and "NOPS_KMS_KEY_ID" in os.environ
+    and os.environ["NOPS_KMS_API_KEY"]
+    and os.environ["NOPS_KMS_KEY_ID"]
+):
+    ENCRYPTED_API_KEY = os.environ["NOPS_KMS_API_KEY"]
+    NOPS_API_KEY = boto3.client("kms", config=boto3_config).decrypt(
+        CiphertextBlob=base64.b64decode(ENCRYPTED_API_KEY), KeyId=os.environ["NOPS_KMS_KEY_ID"]
+    )["Plaintext"]
+    if type(NOPS_API_KEY) is bytes:
+        NOPS_API_KEY = NOPS_API_KEY.decode("utf-8")
+
+elif "NOPS_API_KEY" in os.environ:
+    logger.debug("Fetching the API key from environment variable NOPS_API_KEY")
+    NOPS_API_KEY = os.environ["NOPS_API_KEY"]
+
+NOPS_API_KEY = NOPS_API_KEY.strip()
